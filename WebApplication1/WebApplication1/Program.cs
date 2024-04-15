f337b7d37424209562a86cd84376cd57c0c67799
Jacob Slusser <jslusser.mobile@gmail.com>
2024-04-15 09:48:41 -0700

    Blob post #2!

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -8,6 +8,35 @@ namespace WebApplication1
             var app = builder.Build();
 
             app.MapGet("/", () => """
+                Wait, what?, 4/15/2024
+
+                If you have found this website in its present state, you may be wondering, what is going on? At the moment, not much! LOL.
+                Let me explain. Right now, there is no website. Nothing. This is (nearly) the most minimal web project that can exist.
+                There is no database, there is no static site generator, there isn't even HTML yet -- just plain text.
+
+                In a way, this is the first of many ideas I would like to share, the website itself. Rather than just use an existing technology
+                or blog platform, we will build one! Any mistakes and things I do along the way you will get to see also. It is also a
+                lesson about "just starting". So often, I can get caught up in overplanning things and so this approach forces me to "just start",
+                even though I don't know how it will all go yet.
+
+                So right now, I am writing this in a code editor -- not a nice UI, no spellcheck, nothing. I created this project using
+                Visual Studio > New Project > ASP.NET Core Empty and the posts are literally compiled into the executable and deployed any time
+                I want to update. I haven't even named the project. It still has the default name of WebApplication1.
+
+                My update process looks like this:
+                1. I make my changes in the code,
+                2. Run the following to compile into a docker image: `dotnet publish -t:PublishContainer`,
+                3. Export that docker image to a tar file using: `docker save -o ./webapplication1.tar webapplication1:latest`,
+                4. Copy that to my host Linux machine: `scp webapplication1.tar jacob@192.168.4.81:/home/jacob/website`,
+                5. Import the docker image to the local registry: `sudo docker load -i ./webapplication1.tar`,
+                6. And finally recompose the docker container: `sudo docker compose up -d`.
+
+                It's a whole thing...
+
+                You can see this in the source code which I am also sharing here: https://github.com/jacobslusser/website.
+
+                ---
+
                 Humble Beginnings, 4/14/2024
 
                 Join me as I build a personal website from scratch and share some things that I've learned in my life....
